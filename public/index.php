5b9c40258e3b85d4f4d7154092f5bfcecb2b9ba0
sahil <sahilvernekar96@gmail.com>
2024-09-17 08:54:28 +0530

    Routing - setup

--- public/index.php
+++ public/index.php
@@ -1,11 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello</title>
-</head>
-<body>
-    <h1>chor</h1>
-</body>
-</html>
+<?php
+
+function show($stuff){
+    echo "<pre>";
+    print_r($stuff);
+    echo "</pre>";
+}
+
+function splitURL(){
+    $URL = $_GET['url'] ?? 'home';
+    $URL = explode("/", $URL) ;
+    return $URL;
+}
+
+function loadController(){
+    $URL = splitURL();
+    $filename = "../app/controllers/".ucfirst($URL[0]).".php";
+    if(file_exists($filename))
+    {
+        require $filename;
+    }
+    else{
+        $filename = "../app/controllers/_404.php";
+        require $filename;
+    }
+}
+loadController();

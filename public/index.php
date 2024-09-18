4558ba83b9560a91d5a1fe5ab54236dc75cf39be
sahil <sahilvernekar96@gmail.com>
2024-09-18 22:25:17 +0530

    moving things around

--- public/index.php
+++ public/index.php
@@ -1,27 +1,32 @@
 <?php
 
+session_start();
+class App{
+    
+    private function splitURL(){
+        $URL = $_GET['url'] ?? 'home';
+        $URL = explode("/", $URL) ;
+        return $URL;
+    }
+    
+    private function loadController(){
+        $URL = $this->splitURL();
+        $filename = "../app/controllers/".ucfirst($URL[0]).".php";
+        if(file_exists($filename))
+        {
+            require $filename;
+        }
+        else{
+            $filename = "../app/controllers/_404.php";
+            require $filename;
+        }
+    }
+}
+
 function show($stuff){
     echo "<pre>";
     print_r($stuff);
     echo "</pre>";
 }
 
-function splitURL(){
-    $URL = $_GET['url'] ?? 'home';
-    $URL = explode("/", $URL) ;
-    return $URL;
-}
-
-function loadController(){
-    $URL = splitURL();
-    $filename = "../app/controllers/".ucfirst($URL[0]).".php";
-    if(file_exists($filename))
-    {
-        require $filename;
-    }
-    else{
-        $filename = "../app/controllers/_404.php";
-        require $filename;
-    }
-}
-loadController();
+ApploadController();

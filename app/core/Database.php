e5fc0c928bf3ea20d15ce08e37c03b536da35146
sahil <sahilvernekar96@gmail.com>
2024-09-24 07:58:27 +0530

    test databse connection

--- app/core/Database.php
+++ app/core/Database.php
@@ -1,6 +1,24 @@
 <?php
 
-$string = "mysql:hostname=" . DBHOST . ";dbname=" . DBNAME;
-$con = new PDO($string, DBUSER, DBPASS);
+Trait Database
+{
+    private function connect()
+    {
+        $string = "mysql:hostname=" . DBHOST . ";dbname=" . DBNAME;
+        return $con = new PDO($string, DBUSER, DBPASS);
+    }
 
-show($con);
+    public function query($query, $data = [])
+    {
+        $conn = $this->connect();
+        $stm = $conn->prepare($query);
+        $check = $stm->execute($data);
+        if ($check) {
+            $result = $stm->fetchAll(PDO::FETCH_OBJ);
+            if (is_array($result) && count($result)) {
+                return $result;
+            }
+        }
+        return false;
+    }
+}
--- app/core/Model.php
+++ app/core/Model.php
@@ -1,1 +1,13 @@
 <?php
+
+class Model
+{   
+    use Database;
+    
+    function test()
+    {
+        $query = "select * from users";
+        $result = $this->query($query);
+        show($result);
+    }
+}

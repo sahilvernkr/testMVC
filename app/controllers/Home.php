5b27aae08404f2cca7774ecb392ce8ce28f1a0ad
sahil <sahilvernekar96@gmail.com>
2024-09-30 22:51:52 +0530

    insert and delete - model.php

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,11 +5,13 @@ class Home extends Controller
     public function index($a = '', $b = '', $c = '')
     {
         $model = new Model;
-        $arr['id'] = 1;
-        $arr['name'] = 'tim';
 
-        $result = $model->where($arr);
-        show($result);
+        $arr['name'] = 'zom';
+        $arr['age'] = 13;
+        $arr['date'] = date("Y-m-d");
+        $result = $model->insert($arr);
+        //  $result = $model->delete(3);
+        // show($result);
         echo "This is home controller";
         $this->view('home');
     }
--- app/core/Model.php
+++ app/core/Model.php
@@ -50,9 +50,22 @@ class Model
         return false;
     }
 
-    public function insert($data) {}
+    public function insert($data)
+    {
+        $keys = array_keys($data);
+        $query = "insert into $this->table (" . implode(",", $keys) . ") values (:" . implode(",:", $keys) . ")";
+        echo $query;
+        $this->query($query, $data);
+        return false;
+    }
 
     public function update($id, $data, $id_column = 'id') {}
 
-    public function delete($id, $id_column = 'id') {}
+    public function delete($id, $id_column = 'id')
+    {
+        $data[$id_column] = $id;
+        $query = "delete from $this->table where $id_column = :$id_column";
+        echo $query;
+        $this->query($query, $data);
+    }
 }
